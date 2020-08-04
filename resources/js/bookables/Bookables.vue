1c3e740b6442daa25854ea625668a95d403b6825
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-08-05 04:14:55 +0800

    Component props and prop types

--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <bookable-list-item></bookable-list-item>
+        <bookable-list-item
+        item-title="Cheap Villa"
+        item-content="A very cheap villa"
+        v-bind:price="2000">
+        </bookable-list-item>
+        <bookable-list-item
+        item-title="Cheap Villa 2"
+        item-content="A very cheap villa 2"
+        :price="1000">
+        </bookable-list-item>
     </div>
 </template>
 
