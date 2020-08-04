58c8216da928dbc831b6ee3e819301f54f348bcd
Jason Pis-an <jpisan@Jasons-MacBook-Air.local>
2020-08-05 05:40:18 +0800

    Reactivity gotchas

--- public/js/app.js
+++ public/js/app.js
@@ -1954,6 +1954,11 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
@@ -1961,14 +1966,8 @@ __webpack_require__.r(__webpack_exports__);
   },
   data: function data() {
     return {
-      bookable1: {
-        title: "Cheap Villa",
-        content: "A very cheap villa"
-      },
-      bookable2: {
-        title: "Cheap Villa 2",
-        content: "A very cheap villa 2"
-      }
+      bookable1: null,
+      bookable2: null
     };
   },
   // beforeCreate() {
@@ -1981,9 +1980,27 @@ __webpack_require__.r(__webpack_exports__);
     console.log(this.bookable1);
     console.log(this.bookable2);
     setTimeout(function () {
-      _this.bookable1.title = "Expensive Villa";
-      _this.bookable2.title = "Very Expensive Villa";
+      _this.bookable1 = {
+        title: "Cheap Villa",
+        content: "A very cheap villa"
+      };
+      _this.bookable2 = {
+        title: "Cheap Villa 2",
+        content: "A very cheap villa 2"
+      };
+      _this.bookable3 = {
+        title: "Expensive villa",
+        content: "A very cheap villa 3"
+      };
     }, 5000);
+    setTimeout(function () {
+      console.log('First change');
+      _this.bookable1.title = "You will see this!";
+    }, 8000);
+    setTimeout(function () {
+      console.log('Second change');
+      _this.bookable3.title = "You will not see this!";
+    }, 12000);
   } // beforeMount() {
   //     console.log('before mount');
   // },
@@ -37723,6 +37740,14 @@ var render = function() {
           "item-title": _vm.bookable2.title,
           "item-content": _vm.bookable2.content,
           price: 1000
+        }
+      }),
+      _vm._v(" "),
+      _c("bookable-list-item", {
+        attrs: {
+          "item-title": _vm.bookable3.title,
+          "item-content": _vm.bookable3.content,
+          price: 1500
         }
       })
     ],
